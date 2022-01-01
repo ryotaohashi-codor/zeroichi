462b506aa54855cb67ee5a216571594de08dc1a0
ryota <ohashi@codor.co.jp>
2022-01-01 10:34:11 +0900

    a

--- zeroichi/item/tests.py
+++ zeroichi/item/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+c = Client()
+response = c.get('/')
+response.status_code
+print(response.content)
